32aa3f0e3f7155ee46aa5d03bc9f1bb29cfb8de5
David <davidfdo.garcia@hotmail.com>
2022-07-17 20:25:29 -0500

    Agregar archivos de proyecto.

---
+++ proyecto_tienda/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace proyecto_tienda
+{
+    partial class Tienda
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label productoLabel;
+            System.Windows.Forms.Label descripcionLabel;
+            System.Windows.Forms.Label precioLabel;
+            this.productosDataGridView = new System.Windows.Forms.DataGridView();
+            this.productoTextBox = new System.Windows.Forms.TextBox();
+            this.descripcionTextBox = new System.Windows.Forms.TextBox();
+            this.precioTextBox = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.productosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tiendaDataSet = new proyecto_tienda.tiendaDataSet();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productosTableAdapter = new proyecto_tienda.tiendaDataSetTableAdapters.productosTableAdapter();
+            this.tableAdapterManager = new proyecto_tienda.tiendaDataSetTableAdapters.TableAdapterManager();
+            productoLabel = new System.Windows.Forms.Label();
+            descripcionLabel = new System.Windows.Forms.Label();
+            precioLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.productosDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tiendaDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // productoLabel
+            // 
+            productoLabel.AutoSize = true;
+            productoLabel.Location = new System.Drawing.Point(20, 48);
+            productoLabel.Name = "productoLabel";
+            productoLabel.Size = new System.Drawing.Size(52, 13);
+            productoLabel.TabIndex = 3;
+            productoLabel.Text = "producto:";
+            // 
+            // descripcionLabel
+            // 
+            descripcionLabel.AutoSize = true;
+            descripcionLabel.Location = new System.Drawing.Point(20, 74);
+            descripcionLabel.Name = "descripcionLabel";
+            descripcionLabel.Size = new System.Drawing.Size(64, 13);
+            descripcionLabel.TabIndex = 5;
+            descripcionLabel.Text = "descripcion:";
+            // 
+            // precioLabel
+            // 
+            precioLabel.AutoSize = true;
+            precioLabel.Location = new System.Drawing.Point(20, 100);
+            precioLabel.Name = "precioLabel";
+            precioLabel.Size = new System.Drawing.Size(39, 13);
+            precioLabel.TabIndex = 7;
+            precioLabel.Text = "precio:";
+            // 
+            // productosDataGridView
+            // 
+            this.productosDataGridView.AutoGenerateColumns = false;
+            this.productosDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productosDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.productosDataGridView.DataSource = this.productosBindingSource;
+            this.productosDataGridView.Location = new System.Drawing.Point(216, 45);
+            this.productosDataGridView.Name = "productosDataGridView";
+            this.productosDataGridView.Size = new System.Drawing.Size(455, 220);
+            this.productosDataGridView.TabIndex = 1;
+            // 
+            // productoTextBox
+            // 
+            this.productoTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.productosBindingSource, "producto", true));
+            this.productoTextBox.Location = new System.Drawing.Point(90, 45);
+            this.productoTextBox.Name = "productoTextBox";
+            this.productoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.productoTextBox.TabIndex = 4;
+            // 
+            // descripcionTextBox
+            // 
+            this.descripcionTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.productosBindingSource, "descripcion", true));
+            this.descripcionTextBox.Location = new System.Drawing.Point(90, 71);
+            this.descripcionTextBox.Name = "descripcionTextBox";
+            this.descripcionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.descripcionTextBox.TabIndex = 6;
+            // 
+            // precioTextBox
+            // 
+            this.precioTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.productosBindingSource, "precio", true));
+            this.precioTextBox.Location = new System.Drawing.Point(90, 97);
+            this.precioTextBox.Name = "precioTextBox";
+            this.precioTextBox.Size = new System.Drawing.Size(100, 20);
+            this.precioTextBox.TabIndex = 8;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(23, 149);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(167, 23);
+            this.btnInsertar.TabIndex = 9;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(23, 187);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(167, 23);
+            this.btnModificar.TabIndex = 10;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(23, 226);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(167, 23);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(23, 264);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(167, 23);
+            this.btnBuscar.TabIndex = 12;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // productosBindingSource
+            // 
+            this.productosBindingSource.DataMember = "productos";
+            this.productosBindingSource.DataSource = this.tiendaDataSet;
+            // 
+            // tiendaDataSet
+            // 
+            this.tiendaDataSet.DataSetName = "tiendaDataSet";
+            this.tiendaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "producto";
+            this.dataGridViewTextBoxColumn2.HeaderText = "producto";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "descripcion";
+            this.dataGridViewTextBoxColumn3.HeaderText = "descripcion";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "precio";
+            this.dataGridViewTextBoxColumn4.HeaderText = "precio";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // productosTableAdapter
+            // 
+            this.productosTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.productosTableAdapter = this.productosTableAdapter;
+            this.tableAdapterManager.UpdateOrder = proyecto_tienda.tiendaDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // Tienda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(productoLabel);
+            this.Controls.Add(this.productoTextBox);
+            this.Controls.Add(descripcionLabel);
+            this.Controls.Add(this.descripcionTextBox);
+            this.Controls.Add(precioLabel);
+            this.Controls.Add(this.precioTextBox);
+            this.Controls.Add(this.productosDataGridView);
+            this.Name = "Tienda";
+            this.Text = "Tienda";
+            this.Load += new System.EventHandler(this.Tienda_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.productosDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tiendaDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private tiendaDataSet tiendaDataSet;
+        private System.Windows.Forms.BindingSource productosBindingSource;
+        private tiendaDataSetTableAdapters.productosTableAdapter productosTableAdapter;
+        private tiendaDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView productosDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.TextBox productoTextBox;
+        private System.Windows.Forms.TextBox descripcionTextBox;
+        private System.Windows.Forms.TextBox precioTextBox;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ proyecto_tienda/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_tienda
+{
+    public partial class Tienda : Form
+    {
+        public Tienda()
+        {
+            InitializeComponent();
+        }
+
+        private void productosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productosBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.tiendaDataSet);
+
+        }
+
+        private void Tienda_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'tiendaDataSet.productos' Puede moverla o quitarla según sea necesario.
+            this.productosTableAdapter.Fill(this.tiendaDataSet.productos);
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            this.productosTableAdapter.Insertar(productoTextBox.Text, descripcionTextBox.Text, precioTextBox.Text);
+            this.productosTableAdapter.Fill(this.tiendaDataSet.productos);
+            productoTextBox.Text = "";
+            descripcionTextBox.Text = "";
+            precioTextBox.Text = "";
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            this.productosTableAdapter.Modificar(productoTextBox.Text, descripcionTextBox.Text, precioTextBox.Text, productoTextBox.Text);
+            this.productosTableAdapter.Fill(this.tiendaDataSet.productos);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            this.productosTableAdapter.Eliminar(productoTextBox.Text);
+            this.productosTableAdapter.Fill(this.tiendaDataSet.productos);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            this.productosTableAdapter.Buscar(tiendaDataSet.productos, productoTextBox.Text);
+        }
+    }
+}
